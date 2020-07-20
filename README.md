f2d602eba3c1d98f9ef5b8287be5616d7d358c43
Tarun Kumar Sukhu <tksukhu@gmail.com>
2020-07-20 11:22:10 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# single-spa-es5-alpine-angularjs
+A minimalistic single-spa example with an angularjs and an alpine app. This is builds on top of the sample https://github.com/joeldenning/single-spa-es5-angularjs (it also uses the latest version of the angularjs single spa helper) and is a proof of concept of working with AlpineJS using the single-spa-html helper
+
+## Usage
+```bash
+npm install # or yarn install
+npm start
+```
+
+# credits
+
+- This builds on top of Joel Dennings sample for es5 angular js example  https://github.com/joeldenning/single-spa-es5-angularjs
+- The Alpine JS example is a variation from Jack Herringtons version in this [video](https://www.youtube.com/watch?v=guv7TyeOY3o)
+
---
+++ init-apps.js
@@ -0,0 +1,117 @@
+const es5App = window.singleSpaAngularjs.default({
+  angular: angular,
+  mainAngularModule: "main-module",
+  uiRouter: false,
+  preserveGlobal: true,
+  template: "<root />",
+});
+
+angular.module("main-module", []);
+
+angular.module("main-module").component("root", {
+  template: `<div class="mui-container">
+  <div class="mui-panel">
+    <div class="mui--text-headline">Hello from angularjs!</div>
+  </div>
+  </div>
+  `,
+  controllerAs: "vm",
+  controller: function () {
+    var vm = this;
+
+    vm.$onInit = function () {
+      console.log("mounting root angular component");
+    };
+
+    vm.$onDestroy = function () {
+      console.log("unmounting root angular component!");
+    };
+  },
+});
+
+// es5App is a global variable used in the html file
+const alpineApp = window.singleSpaHtml.default({
+  template: (props) => `
+  <div class="mui-container">
+  <div x-data="{ open: false }">
+  <div class="mui-panel">
+  <div class="mui--test-display1"> Test x-show</div>
+      <button class="mui-btn mui-btn--primary" @click="open = !open">
+        Open/Close
+      </button>
+      <div x-show="open" class="mui--text-display4">
+        Hey, I'm open
+      </div>
+      </div>
+      <div class="mui-panel">
+      <div class="mui--test-display1"> Test x-model input binding</div>
+      <div x-data="{ name: 'Single SPA Integrated AlpineJS'}">
+        <div class="mui-textfield">
+          <input type="text" name="name" id="name" x-model="name" />
+        </div>
+        <div class="mui--test-display2">
+          My name is <span x-text="name"></span>
+        </div>
+      </div>
+      </div>
+      <div class="mui-panel">
+      <div class="mui--test-display1"> TODO App Test </div>
+      <div x-data="{ todos: ['Learn Single-SPA', 'Try AlpineJS'], newTodo: 'Integrate'}">
+        <div class="mui-textfield">
+          <input type="text" name="todo" id="todo" x-model="newTodo" />
+        </div>
+        <button class="mui-btn mui-btn--primary" @click="todos.push(newTodo)">
+          Add
+        </button>
+
+        <table class="mui-table">
+          <thead>
+            <tr>
+              <th>Task</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+          <template x-for="todo in todos" :key="todo">
+            <tr>
+              <td x-text="todo""></td>
+              <td><button class="mui-btn mui-btn--small mui-btn--fab mui-btn--danger"  @click="todos = todos.filter(item => item !== todo)">
+              -
+            </button></td>
+            </tr>
+            </template>
+
+          </tbody>
+        </table>
+        
+      </div>
+    </div>
+    </div>
+    <div class="mui-panel">
+    <div class="mui--test-display1"> Test x-bind to change background color (red/purple) on even/odd numbers </div>
+    <div x-data="{ count: 0}">
+      <div class="mui-textfield">
+        <input type="text" name="count" id="count" x-model="count" />
+      </div>
+      <div x-bind:class="{ 'mui--bg-color-red-500': count %2 === 0, 'mui--bg-color-purple-500': count %2 !== 0}">
+        Big when even
+      </div>
+    </div>
+    </div>
+    </div>
+  `,
+});
+
+singleSpa.registerApplication({
+  name: "my-alpine-app",
+  app: alpineApp,
+  activeWhen: () => window.showAlpineApp, //parseQuery(document.location.search).app === "alpine",
+});
+
+singleSpa.registerApplication({
+  name: "es5-app",
+  app: es5App,
+  activeWhen: () => window.showEs5App, //parseQuery(document.location.search).app === "angularjs",
+});
+
+singleSpa.start();
